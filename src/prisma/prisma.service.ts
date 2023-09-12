5785ec420065a4c70a9ddd3dc068b9156b018997
Andrés Court <andres@andrescourt.com>
2023-09-11 22:04:02 -0500

    BUGFIX: changed hardcoded db call

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -3,13 +3,13 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor() {
-        super({
-            datasources: {
-                db: {
-                    url: "postgresql://andresc:a1s2d3fr@localhost:5432/chingu?schema=public"
-                },
-            }
-        })
-    }
+     constructor() {
+          super({
+               datasources: {
+                    db: {
+                         url: process.env.DATABASE_URL as string
+                    },
+               }
+          })
+     }
 }
