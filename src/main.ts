cd5ff63dcb4a20879a8b3b4a2b098293457a9363
Dan Ko <danko56666@gmail.com>
2023-09-21 19:34:41 -0400

    disable swagger ui in production

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,16 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix("api/v1");
 
-    const config = new DocumentBuilder()
-        .setTitle("Chingu Dashboard Project")
-        .setDescription("The api for chingu dashboard")
-        .setVersion("1.0")
-        .build();
+    if (process.env.NODE_ENV === "development") {
+        const config = new DocumentBuilder()
+            .setTitle("Chingu Dashboard Project")
+            .setDescription("The api for chingu dashboard")
+            .setVersion("1.0")
+            .build();
 
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup("docs", app, document);
+        const document = SwaggerModule.createDocument(app, config);
+        SwaggerModule.setup("docs", app, document);
+    }
 
     await app.listen(process.env.PORT);
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,6 +6,7 @@ export class UserService {
     constructor(private prisma: PrismaService) {}
 
     async getUser() {
+        console.log(process.env.NODE_ENV);
         const users = await this.prisma.user.findMany();
         return users;
     }
