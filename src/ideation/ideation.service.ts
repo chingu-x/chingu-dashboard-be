fbc1e7017c10042640046b0d880becb6e0bb8bb1
Dan Ko <danko56666@gmail.com>
2023-09-21 23:16:49 -0400

    remove console log

--- src/ideation/ideation.service.ts
+++ src/ideation/ideation.service.ts
@@ -60,8 +60,6 @@ export class IdeationService {
             },
         });
 
-        console.log(query.map((q) => Object.values(q).flat()).flat());
-
         return query.map((q) => Object.values(q).flat()).flat();
     }
 
