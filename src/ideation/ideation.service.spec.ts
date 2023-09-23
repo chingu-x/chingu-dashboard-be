c8ba15c49b8f4db2a4e1d044aeed71ef2f39ac1c
Cheryl M <webmaster@cherylli.com>
2023-09-24 01:27:19 +1000

    fix jest test errors

--- src/ideation/ideation.service.spec.ts
+++ src/ideation/ideation.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { IdeationService } from "./ideation.service";
+import {PrismaService} from "../prisma/prisma.service";
 
 describe("IdeationService", () => {
     let service: IdeationService;
 
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
-            providers: [IdeationService],
+            providers: [IdeationService, PrismaService],
         }).compile();
 
         service = module.get<IdeationService>(IdeationService);
--- src/teams/teams.service.spec.ts
+++ src/teams/teams.service.spec.ts
@@ -1,12 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TeamsService } from './teams.service';
+import {PrismaService} from "../prisma/prisma.service";
 
 describe('TeamsService', () => {
   let service: TeamsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TeamsService],
+      providers: [TeamsService, PrismaService],
     }).compile();
 
     service = module.get<TeamsService>(TeamsService);
