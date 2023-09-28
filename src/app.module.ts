9e6f041cc8fcb2701915738f9828febad5673e19
Cheryl M <webmaster@cherylli.com>
2023-09-28 22:39:46 +1000

    add users endpoints so frontend can get user ids for development purpose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from "@nestjs/common";
-import { UserModule } from "./user/user.module";
 import { PrismaModule } from "./prisma/prisma.module";
 import { IdeationModule } from "./ideation/ideation.module";
 import { TeamsModule } from './teams/teams.module';
 import { TechsModule } from './techs/techs.module';
 import { HealthCheckController } from "./HealthCheck/health-check.controller";
 import { HealthCheckService } from "./HealthCheck/health-check.service";
+import { UsersModule } from './users/users.module';
 
 @Module({
-    imports: [UserModule, PrismaModule, IdeationModule, TeamsModule, TechsModule],
+    imports: [UsersModule, PrismaModule, IdeationModule, TeamsModule, TechsModule, UsersModule],
     controllers: [HealthCheckController],
     providers: [HealthCheckService],
 })
---
+++ src/users/users.service.ts
@@ -0,0 +1,85 @@
+import { Injectable } from '@nestjs/common';
+import {PrismaService} from "../prisma/prisma.service";
+
+@Injectable()
+export class UsersService {
+  constructor(private prisma: PrismaService) {
+  }
+
+  findAll() {
+    return this.prisma.user.findMany();
+  }
+
+  userDetailsById(userId: string) {
+    return this.prisma.user.findUnique({
+      where: {
+        id: userId
+      },
+      select: {
+        id:true,
+        firstName: true,
+        lastName: true,
+        avatar: true,
+        githubId: true,
+        discordId: true,
+        twitterId: true,
+        linkedinId: true,
+        email: true,
+        gender: {
+          select: {
+            abbreviation: true,
+            description: true,
+          }
+        },
+        countryCode: true,
+        timezone: true,
+        comment: true,
+        voyageTeamMembers: {
+          select: {
+            id: true,
+            voyageTeam: {
+              select: {
+                name: true,
+                tier: {
+                  select:{
+                    name: true,
+                    description: true
+                  }
+                }
+              }
+            },
+            voyageRole: {
+              select: {
+                name:true,
+                description: true
+              }
+            },
+            status: true,
+            hrPerSprint: true,
+            teamTechStackItemVotes: {
+              select: {
+                id: true,
+                teamTech : {
+                  select : {
+                    tech: {
+                      select: {
+                        id: true,
+                        name: true,
+                        category: {
+                          select : {
+                            name: true,
+                            description: true
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    })
+  }
+}
