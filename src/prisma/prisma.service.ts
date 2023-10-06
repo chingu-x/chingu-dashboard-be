2aaad82a9c69f51909552d0e75febc81f8e2616b
Dan Ko <danko56666@gmail.com>
2023-10-06 19:16:04 -0400

    fix hot reload

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -2,14 +2,4 @@ import { Injectable } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
 
 @Injectable()
-export class PrismaService extends PrismaClient {
-    constructor() {
-        super({
-            datasources: {
-                db: {
-                    url: process.env.DATABASE_URL,
-                },
-            },
-        });
-    }
-}
+export class PrismaService extends PrismaClient {}
