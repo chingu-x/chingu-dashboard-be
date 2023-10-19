44a52139d312ed103cc797e61f7cc347d6d3b195
smurph7894 <104289709+smurph7894@users.noreply.github.com>
2023-10-19 14:27:10 -0700

    create entity class

--- src/ideations/entities/ideation.entity.ts
+++ src/ideations/entities/ideation.entity.ts
@@ -1,1 +1,27 @@
-export class Ideation {}
+import { ApiProperty } from "@nestjs/swagger";
+
+export class Ideation {
+    @ApiProperty()
+    id: number;
+
+    @ApiProperty()
+    userId: number;
+
+    @ApiProperty()
+    title: string;
+
+    @ApiProperty()
+    description: string;
+
+    @ApiProperty()
+    vision: string;
+
+    @ApiProperty()
+    createdAt: Date;
+
+    @ApiProperty()
+    updatedAt: Date;
+
+    @ApiProperty({required: false, nullable: true})
+    projectIdeaVotes: [number] | null;
+}
