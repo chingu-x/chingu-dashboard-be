7c8fe18406da11d0890aa59b39a2ecdf4e71dd84
Curt <121463645+curtwl@users.noreply.github.com>
2023-10-18 22:53:03 -0500

    Add boilerplate for team resources endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { TechsModule } from "./techs/techs.module";
 import { HealthCheckController } from "./HealthCheck/health-check.controller";
 import { HealthCheckService } from "./HealthCheck/health-check.service";
 import { UsersModule } from "./users/users.module";
+import { ResourcesModule } from './resources/resources.module';
 
 @Module({
     imports: [
@@ -15,6 +16,7 @@ import { UsersModule } from "./users/users.module";
         TeamsModule,
         TechsModule,
         UsersModule,
+        ResourcesModule,
     ],
     controllers: [HealthCheckController],
     providers: [HealthCheckService],
---
+++ src/resources/resources.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { ResourcesService } from './resources.service';
+import { CreateResourceDto } from './dto/create-resource.dto';
+import { UpdateResourceDto } from './dto/update-resource.dto';
+
+@Controller('resources')
+export class ResourcesController {
+  constructor(private readonly resourcesService: ResourcesService) {}
+
+  @Post()
+  create(@Body() createResourceDto: CreateResourceDto) {
+    return this.resourcesService.create(createResourceDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.resourcesService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.resourcesService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateResourceDto: UpdateResourceDto) {
+    return this.resourcesService.update(+id, updateResourceDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.resourcesService.remove(+id);
+  }
+}
