66454e12285d0e1519d5627444b3148b7fcaa97e
smurph7894 <104289709+smurph7894@users.noreply.github.com>
2023-10-19 14:26:43 -0700

    remove duplicate info

--- src/ideations/dto/create-ideation-vote.dto.ts
+++ src/ideations/dto/create-ideation-vote.dto.ts
@@ -2,7 +2,6 @@ import { ApiProperty } from "@nestjs/swagger";
 
 export class CreateIdeationVoteDto {
     @ApiProperty()
-    votedBy: string;
     userId: number;
     ProjectIdea: number;
     protectIdeaId: number;
