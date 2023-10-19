209e84776a6d8c8372aab8512255bad231b65e1c
Curt <121463645+curtwl@users.noreply.github.com>
2023-10-19 00:41:37 -0500

    Stub out Prisma calls

--- src/resources/resources.service.ts
+++ src/resources/resources.service.ts
@@ -1,22 +1,33 @@
 import { Injectable } from '@nestjs/common';
 import { CreateResourceDto } from './dto/create-resource.dto';
 import { UpdateResourceDto } from './dto/update-resource.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ResourcesService {
+  constructor(private prisma: PrismaService) {}
+  
   create(createResourceDto: CreateResourceDto) {
-    return 'This action adds a new resource';
+    return this.prisma.teamResource.create({
+      data: createResourceDto,
+    });
   }
 
   findAll(teamId) {
-    return `This action returns all resources`;
+    return this.prisma.teamResource.findMany({
+      where: {}
+    });
   }
 
   update(teamMemberId: number, resourceId: number, updateResourceDto: UpdateResourceDto) {
-    return ``;
+    return this.prisma.teamResources.update({
+      where: {}
+    });
   }
 
   remove(teamMemberId: number, resourceId: number) {
-    return ``;
+    return this.prisma.teamResources.delete({
+
+    });
   }
 }
