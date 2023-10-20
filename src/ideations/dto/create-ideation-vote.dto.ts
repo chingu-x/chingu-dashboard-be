8fad7ecc8a01a24e70f64e42d29e1db3bf023be4
smurph7894 <104289709+smurph7894@users.noreply.github.com>
2023-10-20 13:59:32 -0700

    dto files updated
    validators added

--- src/ideations/dto/create-ideation-vote.dto.ts
+++ src/ideations/dto/create-ideation-vote.dto.ts
@@ -1,8 +1,13 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString} from 'class-validator';
 
 export class CreateIdeationVoteDto {
+    //TODO update userID to uuid once authentication in place.
+    @IsNotEmpty()
     @ApiProperty()
     userId: number;
-    ProjectIdea: number;
-    protectIdeaId: number;
+
+    @IsNotEmpty()
+    @ApiProperty()
+    projectIdeaId: number;
 }
--- src/ideations/dto/create-ideation.dto.ts
+++ src/ideations/dto/create-ideation.dto.ts
@@ -1,5 +1,23 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString} from 'class-validator';
+
+
 export class CreateIdeationDto {
+    //TODO update userID to uuid once authentication in place.
+    @IsNotEmpty()
+    @ApiProperty()
+    userId: number;
+
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty()
     title: string;
+
+    @IsString()
+    @ApiProperty()
     description: string;
+
+    @IsString()
+    @ApiProperty()
     vision: string;
 }
