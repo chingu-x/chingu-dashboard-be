e30e83fe19d370077a54a9217f8a89bb3f008443
Dan Ko <danko56666@gmail.com>
2023-10-21 19:34:48 -0400

    return team id from user detail endpoint

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -38,6 +38,7 @@ export class UsersService {
                         id: true,
                         voyageTeam: {
                             select: {
+                                id: true,
                                 name: true,
                                 tier: {
                                     select: {
