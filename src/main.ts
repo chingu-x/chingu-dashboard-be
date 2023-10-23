0fa98cd4133a5912c841d4c1e1d673ec44c72736
Dan Ko <danko56666@gmail.com>
2023-10-23 13:00:09 -0400

    add cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,15 @@ import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    app.enableCors({
+        origin: [
+            "http://localhost:3000",
+            /^https:\/\/chingu-dashboard-[A-Za-z]+-chingu-dashboard\.vercel\.app$/,
+            "https://chingu-dashboard-git-dev-chingu-dashboard.vercel.app",
+            "https://chingu-dashboard.vercel.app",
+        ],
+        methods: ["GET", "POST", "PATCH", "DELETE"],
+    });
     app.setGlobalPrefix("api/v1");
 
     const config = new DocumentBuilder()
