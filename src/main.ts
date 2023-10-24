e517fc3009f159ca9030dc4023830bba7bc2d936
smurph7894 <104289709+smurph7894@users.noreply.github.com>
2023-10-23 21:56:48 -0700

    Update main.ts
    add validationPipe

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { HttpAdapterHost, NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { PrismaClientExceptionFilter } from "./prisma-client-exception/prisma-client-exception.filter";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.setGlobalPrefix("api/v1");
+
+    app.useGlobalPipes( new ValidationPipe())
 
     const config = new DocumentBuilder()
         .setTitle("Chingu Dashboard Project")
