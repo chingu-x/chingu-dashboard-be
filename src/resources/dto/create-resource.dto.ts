19d6a696fb44af65618a0ac1ab4936a8f0766517
Curt <121463645+curtwl@users.noreply.github.com>
2023-10-25 16:00:50 -0500

    Add class validators

--- src/resources/dto/create-resource.dto.ts
+++ src/resources/dto/create-resource.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString } from "class-validator";
 
 export class CreateResourceDto {
+    @IsString()
+    @IsNotEmpty()
     @ApiProperty()
     url: string;
 
+    @IsString()
+    @IsNotEmpty()
     @ApiProperty()
     title: string;
 }
