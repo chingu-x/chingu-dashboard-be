b171768466e0e439454af0adceac8527c28831ee
smurph7894 <104289709+smurph7894@users.noreply.github.com>
2023-10-24 23:36:13 -0700

    updated delete and createVote dto files
    removed projectIdeaId requirement from create ideationVote
    added dto to deleteIdeation and deleteIdeationVote to move userId to body from parameter

--- src/ideations/dto/delete-ideation-vote.dto.ts
+++ src/ideations/dto/delete-ideation-vote.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsString} from 'class-validator';
+import { IsNotEmpty, IsString, } from 'class-validator';
 
-export class CreateIdeationVoteDto {
+export class DeleteIdeationVoteDto {
     @IsString()
     @IsNotEmpty()
     @ApiProperty()
