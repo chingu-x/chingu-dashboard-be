bc956b4e250393b79bcea66870f955bf890cc0c0
Curt <121463645+curtwl@users.noreply.github.com>
2023-10-25 16:04:54 -0500

    Remove userId from findAllResources response

--- src/resources/resources.service.ts
+++ src/resources/resources.service.ts
@@ -65,7 +65,6 @@ export class ResourcesService {
                                 firstName: true,
                                 lastName: true,
                                 avatar: true,
-                                id: true,
                             },
                         },
                     },
