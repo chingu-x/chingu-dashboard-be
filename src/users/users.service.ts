81279576a5fb435dbbeac0059269b15e8fd98800
Cheryl M <webmaster@cherylli.com>
2023-10-26 22:18:29 +1100

    add a rule to eslint to allow unused variable for the case of rest siblings

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
 import { UserEntity } from "./entities/user.entity";
-import * as bcrypt from "bcrypt";
 
 @Injectable()
 export class UsersService {
