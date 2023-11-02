86a891778c7a81735f443a32462f3ddd3b48861b
Cheryl M <webmaster@cherylli.com>
2023-11-02 22:09:25 +1100

    add select condition to forms

--- src/forms/forms.service.ts
+++ src/forms/forms.service.ts
@@ -1,12 +1,52 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
 
+const formSelect = {
+    id: true,
+    formType: {
+        select: {
+            id: true,
+            name: true,
+        },
+    },
+    title: true,
+    description: true,
+    questions: {
+        select: {
+            id: true,
+            order: true,
+            inputType: {
+                select: {
+                    id: true,
+                    name: true,
+                }
+            },
+            text: true,
+            description: true,
+            answerRequired: true,
+            multipleAllowed: true,
+            optionGroup: {
+                select: {
+                    optionChoices: {
+                        select: {
+                            id: true,
+                            text: true
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 @Injectable()
 export class FormsService {
     constructor(private prisma: PrismaService) {}
 
     getAllForms() {
-        return this.prisma.form.findMany({});
+        return this.prisma.form.findMany({
+            select: formSelect
+        });
     }
 
     async getFormById(formId: number) {
@@ -14,18 +54,7 @@ export class FormsService {
             where: {
                 id: formId,
             },
-            select: {
-                id: true,
-                formType: {
-                    select: {
-                        id: true,
-                        name: true,
-                    },
-                },
-                title: true,
-                description: true,
-                questions: true,
-            },
+            select: formSelect
         });
         if (!form)
             throw new NotFoundException(
