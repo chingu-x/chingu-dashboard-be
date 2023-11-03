fbef834819f320f9b2275f53d3fac3354def1ab1
Curt <121463645+curtwl@users.noreply.github.com>
2023-11-03 17:58:02 -0500

    Merge changes from dev

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -19,16 +19,6 @@ export class AuthController {
 
     @UseGuards(LocalAuthGuard)
     @Post("login")
-<<<<<<< HEAD
-    async login(@Request() req, @Res({ passthrough: true }) res) {
-        const access_token = await this.authService.login(req.user);
-        res.cookie("access_token", access_token.access_token, {
-            expires: new Date(Date.now() + 60 * 60 * 7 * 24),
-            httpOnly: true,
-            secure: true,
-        });
-        return access_token;
-=======
     async login(
         @Body() body: LoginDto,
         @Request() req,
@@ -46,7 +36,6 @@ export class AuthController {
             console.log(e);
             throw new UnauthorizedException("Login Error");
         }
->>>>>>> origin/dev
     }
 
     @Post("logout")
