33920e219481e150658603daac5589c5526795c8
Jennifer Alexander <jenny_alexander@icloud.com>
2023-11-09 12:48:51 -0600

    Add @IsInt validation to hours per sprint

--- src/teams/dto/update-team-member.dto.ts
+++ src/teams/dto/update-team-member.dto.ts
@@ -1,6 +1,8 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsInt } from "class-validator";
 
 export class UpdateTeamMemberDto {
+    @IsInt()
     @ApiProperty()
     hrPerSprint: number;
 }
