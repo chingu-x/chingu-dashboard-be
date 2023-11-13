a98c91f0306e55c5ea31e16a757df45e2b627eba
smurph7894 <104289709+smurph7894@users.noreply.github.com>
2023-11-13 15:50:20 -0800

    Switch validate loggedIn and team to global module
    - verified logged in and team member so may fit better in global service since not single responsibility.

--- src/global/global.service.ts
+++ src/global/global.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class GlobalService {}
+export class GlobalService {
+    constructor(private prisma: PrismaService) {}
+
+    //verifies user is logged in by using uuid from cookie and teamId to pull a teamMember. 
+    public async validateLoggedInAndTeamMember(teamId: number, uuid: any){
+        const teamMember = await this.prisma.voyageTeamMember.findFirst({
+            where: {
+                voyageTeamId: teamId,
+                userId: uuid,
+            },
+            select: {
+                id: true,
+            }, 
+        });
+
+        if (!teamMember) {
+            throw new NotFoundException(
+                `TeamId (id: ${teamId}) and/or loggedIn userId (id: ${uuid}) is invalid.`,
+            );
+        }
+        return teamMember;
+    }
+
+
+}
