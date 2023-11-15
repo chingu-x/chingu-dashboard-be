f4c7269623caf6f65bf18565e3ddd6269dbe5c8c
Curt <121463645+curtwl@users.noreply.github.com>
2023-11-15 17:49:46 -0600

    Remove unused imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "./prisma/prisma.module";
-import { IdeationsModule } from "./ideations/ideations.module";
 import { TeamsModule } from "./teams/teams.module";
-import { TechsModule } from "./techs/techs.module";
 import { HealthCheckController } from "./HealthCheck/health-check.controller";
 import { HealthCheckService } from "./HealthCheck/health-check.service";
 import { UsersModule } from "./users/users.module";
-import { ResourcesModule } from "./resources/resources.module";
 import { AuthModule } from "./auth/auth.module";
-import { FeaturesModule } from "./features/features.module";
 import { GlobalModule } from "./global/global.module";
 
 @Module({
