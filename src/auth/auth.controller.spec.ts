1d1adab9b5db708f362842fd85c8750dcd59dc51
kennytram <kennyvtram@gmail.com>
2023-11-20 00:52:53 -0800

    add services

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
 
 describe("AuthController", () => {
     let controller: AuthController;
@@ -7,6 +8,7 @@ describe("AuthController", () => {
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             controllers: [AuthController],
+            providers: [{ provide: AuthService, useValue: {} }],
         }).compile();
 
         controller = module.get<AuthController>(AuthController);
--- src/sprints/sprints.service.spec.ts
+++ src/sprints/sprints.service.spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from "@nestjs/testing";
 import { SprintsService } from "./sprints.service";
 import { PrismaService } from "../prisma/prisma.service";
 import { GlobalService } from "../global/global.service";
+import { FormsService } from "../forms/forms.service";
 
 describe("SprintsService", () => {
     let service: SprintsService;
@@ -15,6 +16,7 @@ describe("SprintsService", () => {
                     useValue: {},
                 },
                 GlobalService,
+                FormsService,
             ],
         }).compile();
 
