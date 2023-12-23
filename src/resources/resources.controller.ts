c5fc7ba833873434d4d0c4600250c5e3b6123d40
Curt <121463645+curtwl@users.noreply.github.com>
2023-12-22 21:25:36 -0600

    Add @ApiResponse tags to resources controller

--- src/resources/resources.controller.ts
+++ src/resources/resources.controller.ts
@@ -9,12 +9,28 @@ import {
     ParseIntPipe,
     UseGuards,
     Request,
+    HttpStatus,
 } from "@nestjs/common";
-import { ApiBearerAuth, ApiOperation, ApiTags } from "@nestjs/swagger";
+import {
+    ApiBearerAuth,
+    ApiOperation,
+    ApiParam,
+    ApiResponse,
+    ApiTags,
+} from "@nestjs/swagger";
 import { ResourcesService } from "./resources.service";
 import { CreateResourceDto } from "./dto/create-resource.dto";
 import { UpdateResourceDto } from "./dto/update-resource.dto";
 import { JwtAuthGuard } from "../auth/jwt-auth.guard";
+import {
+    BadRequestErrorResponse,
+    NotFoundErrorResponse,
+    UnauthorizedErrorResponse,
+} from "src/global/responses/errors";
+import {
+    TeamResourceAddedByResponse,
+    TeamResourceResponse,
+} from "./resources.response";
 
 @Controller()
 @ApiTags("Voyage - Resources")
@@ -24,6 +40,22 @@ export class ResourcesController {
     @ApiOperation({
         summary:
             "Adds a URL with title to the team's resources, addedBy: teamMemberId (int)",
+    })
+    @ApiResponse({
+        status: HttpStatus.CREATED,
+        description: "The resource has been created successfully.",
+        type: TeamResourceResponse,
+    })
+    @ApiResponse({
+        status: HttpStatus.NOT_FOUND,
+        description: "Invalid teamId",
+        type: NotFoundErrorResponse,
+    })
+    @ApiParam({
+        name: "teamId",
+        required: true,
+        description: "voyage team ID",
+        example: 1,
     })
     @UseGuards(JwtAuthGuard)
     @ApiBearerAuth()
@@ -43,6 +75,17 @@ export class ResourcesController {
     @ApiOperation({
         summary: "Gets all resources added by a team given a teamId (int)",
     })
+    @ApiResponse({
+        status: HttpStatus.OK,
+        description: "Returns all the resources for a particular team",
+        isArray: true,
+        type: TeamResourceAddedByResponse,
+    })
+    @ApiResponse({
+        status: HttpStatus.NOT_FOUND,
+        description: "Invalid teamId",
+        type: NotFoundErrorResponse,
+    })
     @UseGuards(JwtAuthGuard)
     @ApiBearerAuth()
     @Get()
@@ -56,6 +99,26 @@ export class ResourcesController {
     @ApiOperation({
         summary:
             "Edit URL/title for a resource if teamMemberId (int) matches logged in user",
+    })
+    @ApiResponse({
+        status: HttpStatus.OK,
+        description: "Resource was successfully updated",
+        type: TeamResourceResponse,
+    })
+    @ApiResponse({
+        status: HttpStatus.UNAUTHORIZED,
+        description: "User is unauthorized to perform this action",
+        type: UnauthorizedErrorResponse,
+    })
+    @ApiResponse({
+        status: HttpStatus.BAD_REQUEST,
+        description: "Bad Request - Resource couldn't be updated",
+        type: BadRequestErrorResponse,
+    })
+    @ApiResponse({
+        status: HttpStatus.NOT_FOUND,
+        description: "Invalid teamId",
+        type: NotFoundErrorResponse,
     })
     @UseGuards(JwtAuthGuard)
     @ApiBearerAuth()
@@ -75,6 +138,26 @@ export class ResourcesController {
     @ApiOperation({
         summary:
             "Delete a resource if teamMemberId (int) matches logged in user",
+    })
+    @ApiResponse({
+        status: HttpStatus.OK,
+        description: "Resource was successfully deleted",
+        type: TeamResourceResponse,
+    })
+    @ApiResponse({
+        status: HttpStatus.UNAUTHORIZED,
+        description: "User is unauthorized to perform this action",
+        type: UnauthorizedErrorResponse,
+    })
+    @ApiResponse({
+        status: HttpStatus.BAD_REQUEST,
+        description: "Bad Request - Resource couldn't be deleted",
+        type: BadRequestErrorResponse,
+    })
+    @ApiResponse({
+        status: HttpStatus.NOT_FOUND,
+        description: "Invalid teamId",
+        type: NotFoundErrorResponse,
     })
     @UseGuards(JwtAuthGuard)
     @ApiBearerAuth()
