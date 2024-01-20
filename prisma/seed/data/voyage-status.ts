f93a61bd8c5ca3fe8d1e154f53c8dd8bbc57600f
Cheryl M <webmaster@cherylli.com>
2024-01-20 15:54:49 +1100

    add voyage status and return in /me endpoint

--- prisma/seed/data/voyage-status.ts
+++ prisma/seed/data/voyage-status.ts
@@ -8,4 +8,18 @@ export default [
         description:
             "member has dropped from the voyage or the team has disbanded",
     },
+    {
+        name: "Inactive",
+        description:
+            "member is inactive or the team is inactive. " +
+            "If a voyage status is inactive, member will still be able to access it",
+    },
+    {
+        name: "Upcoming",
+        description: "Upcoming voyage",
+    },
+    {
+        name: "Locked",
+        description: "Voyage is locked, read only mode",
+    },
 ];
