ac1bbbe23feb6e5ef08347dd307b3352a4552a64
Cheryl M <webmaster@cherylli.com>
2024-02-15 13:42:33 +1100

    fix seeding in tests

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { INestApplication } from "@nestjs/common";
 import { Test, TestingModule } from "@nestjs/testing";
 import { AppModule } from "../src/app.module";
-import { PrismaService } from "../src/prisma/prisma.service";
+// import { PrismaService } from "../src/prisma/prisma.service";
 import { seed } from "../prisma/seed/seed";
 
 describe("AuthController e2e Tests", () => {
