354b2edc24f7b796fd991e82d57882f61a866de2
timDeHof <ttdehof@gmail.com>
2024-03-07 23:04:36 -0500

    feat(revoke-refresh-token.dto.ts): change filename to revoke-refresh-token from revoke
    The filename change was for easier readability and two more class validator were added to the properties: userId and email

--- src/auth/dto/revoke-refresh-token.dto.ts
+++ src/auth/dto/revoke-refresh-token.dto.ts
@@ -1,14 +1,18 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsOptional } from "class-validator";
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class RevokeRTDTo {
     @IsOptional()
+    @IsString()
+    @IsNotEmpty()
     @ApiProperty({
         example: "ebf84db9-3c8d-4900-a1eb-6a0050e983bb",
     })
     userId?: string;
 
     @IsOptional()
+    @IsString()
+    @IsNotEmpty()
     @ApiProperty({
         example: "elza59@ethereal.email",
     })
