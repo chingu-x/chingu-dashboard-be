3984ddb8f2b147b1a5e8747234acd79f3fc32c43
timDeHof <ttdehof@gmail.com>
2024-03-07 22:03:32 -0500

    chore(revoke.dto.ts): add IsOptional decorator to userId and email properties for better validation flexibility The IsOptional decorator has been added to the userId and email properties in the RevokeRTDTo class. This allows these properties to be optional during validation, providing better flexibility in the validation process.

--- src/auth/dto/revoke.dto.ts
+++ src/auth/dto/revoke.dto.ts
@@ -1,11 +1,14 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsOptional } from "class-validator";
 
 export class RevokeRTDTo {
+    @IsOptional()
     @ApiProperty({
         example: "ebf84db9-3c8d-4900-a1eb-6a0050e983bb",
     })
     userId?: string;
 
+    @IsOptional()
     @ApiProperty({
         example: "elza59@ethereal.email",
     })
