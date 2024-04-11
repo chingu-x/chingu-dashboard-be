260dc87c9bc5da2deb62d446cc5ec3bc8d8f133d
siasktv <biankmbm92@gmail.com>
2024-04-10 21:21:08 -0400

    Update TechItemResponse ID in techs.response.ts

--- src/techs/techs.response.ts
+++ src/techs/techs.response.ts
@@ -54,12 +54,12 @@ export class TeamTechResponse {
 
 export class TechItemResponse {
     @ApiProperty({ example: 10 })
-    id: number;
+    teamTechStackItemVoteId: number;
 
     @ApiProperty({ example: 11 })
     teamTechId: number;
 
-    @ApiProperty({ example: "Frontend" })
+    @ApiProperty({ example: 4 })
     teamMemberId: number;
 
     @ApiProperty({ example: "2023-12-01T13:55:00.611Z" })
