7c3ad8ee989caa61460a13950e2c6ec6444760e0
Arman Kumar Jena <arman.k.jena07@gmail.com>
2024-06-10 08:41:19 +0530

    updated swagger doc for 403 and 401

--- src/teams/teams.controller.ts
+++ src/teams/teams.controller.ts
@@ -17,6 +17,7 @@ import {
     VoyageTeamResponse,
 } from "./teams.response";
 import {
+    ForbiddenErrorResponse,
     NotFoundErrorResponse,
     UnauthorizedErrorResponse,
 } from "../global/responses/errors";
@@ -38,6 +39,16 @@ export class TeamsController {
         description: "Successfully gets all voyage teams",
         type: VoyageTeamResponse,
         isArray: true,
+    })
+    @ApiResponse({
+        status: HttpStatus.UNAUTHORIZED,
+        description: "unauthorized access - user is not logged in",
+        type: UnauthorizedErrorResponse,
+    })
+    @ApiResponse({
+        status: HttpStatus.FORBIDDEN,
+        description: "forbidden - user does not have the required permission",
+        type: ForbiddenErrorResponse,
     })
     @CheckAbilities({ action: Action.Manage, subject: "all" })
     @Get()
@@ -68,6 +79,16 @@ export class TeamsController {
         type: "Integer",
         required: true,
         example: 1,
+    })
+    @ApiResponse({
+        status: HttpStatus.UNAUTHORIZED,
+        description: "unauthorized access - user is not logged in",
+        type: UnauthorizedErrorResponse,
+    })
+    @ApiResponse({
+        status: HttpStatus.FORBIDDEN,
+        description: "forbidden - user does not have the required permission",
+        type: ForbiddenErrorResponse,
     })
     @CheckAbilities({ action: Action.Manage, subject: "all" })
     @Get("voyages/:voyageId")
