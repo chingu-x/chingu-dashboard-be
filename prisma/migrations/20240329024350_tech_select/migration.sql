e014b3cfaa6ffa5bbea5a31295351cd207f9ba8a
timDeHof <ttdehof@gmail.com>
2024-07-15 19:26:29 -0400

    chore(migrations): squash migrations

--- prisma/migrations/20240329024350_tech_select/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "TeamTechStackItem" ADD COLUMN     "isSelected" BOOLEAN NOT NULL DEFAULT false;
--- prisma/migrations/20240512035506_add_member_id_to_tech_stack_item_table/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "TeamTechStackItem" ADD COLUMN     "voyageTeamMemberId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "TeamTechStackItem" ADD CONSTRAINT "TeamTechStackItem_voyageTeamMemberId_fkey" FOREIGN KEY ("voyageTeamMemberId") REFERENCES "VoyageTeamMember"("id") ON DELETE SET NULL ON UPDATE CASCADE;
