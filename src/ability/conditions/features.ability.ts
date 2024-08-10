ed9f59eec6f26e0e57649ccf732ad93b746d9363
Arman Kumar Jena <arman.k.jena07@gmail.com>
2024-08-10 23:21:59 +0530

    added features ability

---
+++ src/ability/conditions/features.ability.ts
@@ -0,0 +1,30 @@
+import { UserReq } from "../../global/types/CustomRequest";
+import prisma from "../../prisma/client";
+import { ForbiddenException, NotFoundException } from "@nestjs/common";
+
+export const manageOwnFeaturesById = async (
+    user: UserReq,
+    featureId: number,
+) => {
+    if (user.roles?.includes("admin")) return;
+    const feature = await prisma.projectFeature.findUnique({
+        where: {
+            id: featureId,
+        },
+    });
+    if (!feature) {
+        throw new NotFoundException(
+            `Project Feature (id:${featureId}) not found`,
+        );
+    }
+
+    if (
+        !user.voyageTeams
+            .map((vt) => vt.memberId)
+            .includes(feature.teamMemberId!)
+    ) {
+        throw new ForbiddenException(
+            "Features access control: You can only manage your own project features.",
+        );
+    }
+};
