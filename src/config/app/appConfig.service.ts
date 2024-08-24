34fd5831be4eece6d9ee64c43181c9c720b27c2d
timDeHof <ttdehof@gmail.com>
2024-08-23 22:37:33 -0400

    refactor(appConfig.service.ts): enhance type safety by defining AppConfig interface for configuration service The AppConfig interface is introduced to improve type safety when accessing application configuration properties. This change ensures that the configuration service is aware of the expected structure of the configuration, allowing for better inference and reducing the risk of runtime errors.

--- src/config/app/appConfig.service.ts
+++ src/config/app/appConfig.service.ts
@@ -1,14 +1,21 @@
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 
+interface AppConfig {
+    app: {
+        nodeEnv: string;
+        port: number;
+    };
+}
+
 @Injectable()
 export class AppConfigService {
-    constructor(private readonly configService: ConfigService) {}
+    constructor(private readonly configService: ConfigService<AppConfig>) {}
     get nodeEnv() {
-        return this.configService.get("app.nodeEnv");
+        return this.configService.get("app.nodeEnv", { infer: true })!;
     }
 
     get appPort() {
-        return this.configService.get("app.port");
+        return this.configService.get("app.port", { infer: true })!;
     }
 }
