46c5f7d962953723e6b7b729bfa06cefbd1bc4f8
timDeHof <ttdehof@gmail.com>
2024-08-26 22:21:20 -0400

    refactor(emails/sendEmail.ts): convert standalone email functions to a class-based EmailService for better organization and dependency injection The email sending functions are refactored into an EmailService class, which allows for better organization of related functionality and facilitates dependency injection for configuration services. This change enhances maintainability and testability of the email sending logic by encapsulating the Mailjet client and configuration within a dedicated service.

--- src/utils/emails/sendEmail.ts
+++ src/utils/emails/sendEmail.ts
@@ -1,71 +1,78 @@
+import { Injectable } from "@nestjs/common";
 import { templateIds } from "./templateIds";
 import * as Mailjet from "node-mailjet";
-import * as process from "process";
+import { MailConfigService } from "../../config/mail/mailConfig.service";
+import { AppConfigService } from "../../config/app/appConfig.service";
 
-const mailjet = new Mailjet.Client({
-    apiKey: process.env.MJ_APIKEY_PUBLIC,
-    apiSecret: process.env.MJ_APIKEY_PRIVATE,
-});
-
-export const sendSignupVerificationEmail = async (
-    email: string,
-    token: string,
-) => {
-    if (process.env.NODE_ENV === "test") return;
-    await mailjet.post("send", { version: "v3.1" }).request({
-        Messages: [
-            {
-                To: [
-                    {
-                        Email: email,
+@Injectable()
+export class EmailService {
+    private mailjet: Mailjet.Client;
+    constructor(
+        private readonly ConfigService: MailConfigService,
+        private readonly AppConfigService: AppConfigService,
+    ) {
+        this.mailjet = new Mailjet.Client({
+            apiKey: this.ConfigService.MailjetApiPublic,
+            apiSecret: this.ConfigService.MailjetApiPrivate,
+        });
+    }
+    async sendSignupVerificationEmail(email: string, token: string) {
+        if (this.AppConfigService.nodeEnv === "test") return;
+        await this.mailjet.post("send", { version: "v3.1" }).request({
+            Messages: [
+                {
+                    To: [
+                        {
+                            Email: email,
+                        },
+                    ],
+                    TemplateID: templateIds.verificationEmail,
+                    TemplateLanguage: true,
+                    Variables: {
+                        verificationLink: `${this.ConfigService.FrontendUrl}/users/verify?token=${token}`,
                     },
-                ],
-                TemplateID: templateIds.verificationEmail,
-                TemplateLanguage: true,
-                Variables: {
-                    verificationLink: `${process.env.FRONTEND_URL}/users/verify?token=${token}`,
                 },
-            },
-        ],
-    });
-};
+            ],
+        });
+    }
 
-export const sendAttemptedRegistrationEmail = async (email: string) => {
-    if (process.env.NODE_ENV === "test") return;
-    await mailjet.post("send", { version: "v3.1" }).request({
-        Messages: [
-            {
-                To: [
-                    {
-                        Email: email,
+    async sendAttemptedRegistrationEmail(email: string) {
+        if (this.AppConfigService.nodeEnv === "test") return;
+        await this.mailjet.post("send", { version: "v3.1" }).request({
+            Messages: [
+                {
+                    To: [
+                        {
+                            Email: email,
+                        },
+                    ],
+                    TemplateID: templateIds.attemptRegistrationEmail,
+                    TemplateLanguage: true,
+                    Variables: {
+                        userEmail: email,
+                        passwordResetPage: `${this.ConfigService.FrontendUrl}/users/reset-password`,
                     },
-                ],
-                TemplateID: templateIds.attemptRegistrationEmail,
-                TemplateLanguage: true,
-                Variables: {
-                    userEmail: email,
-                    passwordResetPage: `${process.env.FRONTEND_URL}/users/reset-password`,
                 },
-            },
-        ],
-    });
-};
+            ],
+        });
+    }
 
-export const sendPasswordResetEmail = async (email: string, token: string) => {
-    await mailjet.post("send", { version: "v3.1" }).request({
-        Messages: [
-            {
-                To: [
-                    {
-                        Email: email,
+    async sendPasswordResetEmail(email: string, token: string) {
+        await this.mailjet.post("send", { version: "v3.1" }).request({
+            Messages: [
+                {
+                    To: [
+                        {
+                            Email: email,
+                        },
+                    ],
+                    TemplateID: templateIds.passwordResetEmail,
+                    TemplateLanguage: true,
+                    Variables: {
+                        passwordResetLink: `${this.ConfigService.FrontendUrl}/users/reset-password?token=${token}`,
                     },
-                ],
-                TemplateID: templateIds.passwordResetEmail,
-                TemplateLanguage: true,
-                Variables: {
-                    passwordResetLink: `${process.env.FRONTEND_URL}/users/reset-password?token=${token}`,
                 },
-            },
-        ],
-    });
-};
+            ],
+        });
+    }
+}
