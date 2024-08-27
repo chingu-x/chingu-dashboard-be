b978bc2523b15c3e185c71fc26af860b25ed5215
timDeHof <ttdehof@gmail.com>
2024-08-26 22:24:09 -0400

    test(auth.service.spec.ts): add unit tests for AppConfigService and EmailService to ensure proper dependency injection The unit tests now include checks for the AppConfigService and EmailService to ensure they are properly defined and injected into the AuthService. This enhances the test coverage and ensures that all dependencies are correctly set up, which is crucial for maintaining the integrity of the service.

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -4,13 +4,30 @@ import { PrismaService } from "../prisma/prisma.service";
 import { GlobalService } from "../global/global.service";
 import { UsersService } from "../users/users.service";
 import { JwtService } from "@nestjs/jwt";
+import { AppConfigService } from "../config/app/appConfig.service";
+import { EmailService } from "../utils/emails/sendEmail";
 
 describe("AuthService", () => {
     let service: AuthService;
-
+    let config: AppConfigService;
+    let emailService: EmailService;
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             providers: [
+                {
+                    provide: AppConfigService,
+                    useValue: {
+                        nodeEnv: jest.fn((key: string) =>
+                            key === "NODE_ENV" ? "development" : undefined,
+                        ),
+                    },
+                },
+                {
+                    provide: EmailService,
+                    useValue: {
+                        sendEmail: jest.fn(),
+                    },
+                },
                 AuthService,
                 {
                     provide: PrismaService,
@@ -21,11 +38,18 @@ describe("AuthService", () => {
                 JwtService,
             ],
         }).compile();
-
+        config = module.get<AppConfigService>(AppConfigService);
+        emailService = module.get<EmailService>(EmailService);
         service = module.get<AuthService>(AuthService);
     });
 
     it("should be defined", () => {
         expect(service).toBeDefined();
+    });
+    it("should be defiined", () => {
+        expect(config).toBeDefined();
+    });
+    it("should be defiined", () => {
+        expect(emailService).toBeDefined();
     });
 });
