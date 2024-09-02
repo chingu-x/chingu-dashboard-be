310c1a7ab9b211536940fe9fb44a48b35268a53a
timDeHof <ttdehof@gmail.com>
2024-09-01 20:14:54 -0400

    test(auth.controller.spec.ts): add AuthConfigService to the test module and verify its definition The AuthConfigService is now included in the test module to ensure that it is properly instantiated and available for testing. This change enhances the test coverage by verifying that the AuthConfigService is defined, which is crucial for the functionality of the AuthController.

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,10 +1,10 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
-
+import { AuthConfigService } from "../config/auth/authConfig.service";
 describe("AuthController", () => {
     let controller: AuthController;
-
+    let authConfig: AuthConfigService;
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             controllers: [AuthController],
@@ -13,13 +13,21 @@ describe("AuthController", () => {
                     provide: AuthService,
                     useValue: {},
                 },
+                {
+                    provide: AuthConfigService,
+                    useValue: {},
+                },
             ],
         }).compile();
 
         controller = module.get<AuthController>(AuthController);
+        authConfig = module.get<AuthConfigService>(AuthConfigService);
     });
 
     it("should be defined", () => {
         expect(controller).toBeDefined();
+    });
+    it("should be defined", () => {
+        expect(authConfig).toBeDefined();
     });
 });
