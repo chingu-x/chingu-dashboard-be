e797e4ea8f2e26645125bdbbef36ea3f116849ee
timDeHof <ttdehof@gmail.com>
2024-09-01 20:13:25 -0400

    fix(auth): inject AuthConfigService to retrieve JWT secrets for improved security feat(auth): throw an error if authentication secrets are not found in configuration The AuthConfigService is now injected into the AtStrategy class to retrieve JWT secrets, enhancing security by centralizing secret management. An error is thrown if the secrets are not found, ensuring that the application fails fast and avoids running with invalid configurations. This change improves the robustness of the authentication strategy.

--- src/auth/strategies/at.strategy.ts
+++ src/auth/strategies/at.strategy.ts
@@ -3,17 +3,25 @@ import { ExtractJwt, Strategy } from "passport-jwt";
 import { Request } from "express";
 import { Injectable } from "@nestjs/common";
 import { UsersService } from "../../users/users.service";
-
+import { AuthConfigService } from "src/config/auth/authConfig.service";
 @Injectable()
 export class AtStrategy extends PassportStrategy(Strategy, "jwt-at") {
-    constructor(private usersService: UsersService) {
+    constructor(
+        private usersService: UsersService,
+        private authConfigService: AuthConfigService,
+    ) {
+        const secrets = authConfigService.getSecrets();
+        if (!secrets) {
+            throw new Error("Auth secrets not found in configuration");
+        }
+
         super({
             jwtFromRequest: ExtractJwt.fromExtractors([
                 AtStrategy.extractJWT,
                 ExtractJwt.fromAuthHeaderAsBearerToken(),
             ]),
             ignoreExpiration: false,
-            secretOrKey: process.env.AT_SECRET,
+            secretOrKey: secrets.at,
         });
     }
 
