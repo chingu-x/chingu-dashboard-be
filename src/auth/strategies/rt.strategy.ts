68f468bd2cb6bd8f3988ff88d9d0ea2e93ced9f6
timDeHof <ttdehof@gmail.com>
2024-09-01 20:13:58 -0400

    fix(rt.strategy.ts): inject AuthConfigService to retrieve JWT secrets for refresh token strategy feat(rt.strategy.ts): add error handling for missing authentication secrets in configuration The AuthConfigService is now injected into the RtStrategy constructor to retrieve the JWT secrets dynamically. This change enhances security by ensuring that the secret is not hardcoded and adds error handling to prevent the application from starting if the required secrets are not found in the configuration.

--- src/auth/strategies/rt.strategy.ts
+++ src/auth/strategies/rt.strategy.ts
@@ -2,17 +2,22 @@ import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { Request } from "express";
 import { Injectable } from "@nestjs/common";
+import { AuthConfigService } from "src/config/auth/authConfig.service";
 
 @Injectable()
 export class RtStrategy extends PassportStrategy(Strategy, "jwt-refresh") {
-    constructor() {
+    constructor(private authConfigService: AuthConfigService) {
+        const secrets = authConfigService.getSecrets();
+        if (!secrets) {
+            throw new Error("Auth secrets not found in configuration");
+        }
         super({
             jwtFromRequest: ExtractJwt.fromExtractors([
                 RtStrategy.extractJWT,
                 ExtractJwt.fromAuthHeaderAsBearerToken(),
             ]),
             ignoreExpiration: false,
-            secretOrKey: process.env.RT_SECRET,
+            secretOrKey: secrets.rt,
             passReqToCallback: true,
         });
     }
