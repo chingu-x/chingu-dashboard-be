34f5ffa2c467b9e1fbe54820d6303df3e4cb9c56
timDeHof <ttdehof@gmail.com>
2024-09-03 22:30:18 -0400

    feat(prisma): integrate ConfigModule for dynamic database configuration
    The PrismaModule now imports the ConfigModule and provides a factory
    to create a database configuration using the DATABASE_URL from the
    environment. This allows for more flexible database connection
    management, enabling the application to adapt to different environments
    without hardcoding the database URL. The PrismaService is updated to
    use this dynamic configuration, improving maintainability and
    configurability of the database connection.

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,9 +1,23 @@
 import { Global, Module } from "@nestjs/common";
 import { PrismaService } from "./prisma.service";
+import { DbConfig } from "../config/database/dbConfig.interface";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
 @Global()
 @Module({
-    providers: [PrismaService],
+    imports: [ConfigModule],
+    providers: [
+        PrismaService,
+        {
+            provide: "DB-Config",
+            useFactory: (configService: ConfigService): DbConfig => ({
+                db: {
+                    url: configService.get<string>("DATABASE_URL") as string,
+                },
+            }),
+            inject: [ConfigService],
+        },
+    ],
     exports: [PrismaService],
 })
 export class PrismaModule {}
