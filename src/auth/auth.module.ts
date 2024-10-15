a4911075980bf6c5ee18e269571e04058b5b99c0
timDeHof <ttdehof@gmail.com>
2024-10-14 23:09:33 -0400

    refactor(auth.controller.ts, auth.module.ts): replace relative paths with absolute paths for better readability and maintainability The changes were made to replace relative paths with absolute paths. This makes the code more readable and easier to maintain, as it's easier to understand where the imported modules are coming from. It also reduces the risk of errors when files are moved around in the project structure.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import { Module } from "@nestjs/common";
 import { AuthService } from "./auth.service";
-import { UsersModule } from "../users/users.module";
+import { UsersModule } from "@/users/users.module";
 import { AuthController } from "./auth.controller";
 import { PassportModule } from "@nestjs/passport";
 import { LocalStrategy } from "./strategies/local.strategy";
