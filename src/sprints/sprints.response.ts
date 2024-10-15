73ecc6d836de30d20f830de03f50e90ed5ddec34
timDeHof <ttdehof@gmail.com>
2024-10-14 21:52:37 -0400

    refactor(sprints): change relative import paths to absolute paths for better maintainability The relative import paths in the sprints module, controller, service, and response files have been changed to absolute paths. This makes the code more maintainable and easier to navigate, as it is now clear where the imported modules are located in the project structure.

--- src/sprints/sprints.response.ts
+++ src/sprints/sprints.response.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { Optional } from "@nestjs/common";
-import { ResponseGroup } from "../forms/forms.response";
+import { ResponseGroup } from "@/forms/forms.response";
 
 class Sprint {
     @ApiProperty({ example: 1 })
