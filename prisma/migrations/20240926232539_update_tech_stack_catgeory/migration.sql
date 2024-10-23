49af84a806c2b6b5c3f1269433e4b71636c48a01
Josh Hinman <joshuahinman@gmail.com>
2024-10-23 12:15:05 -0700

    revised categories schema , migrations

--- prisma/migrations/20240926232539_update_tech_stack_catgeory/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropIndex
-DROP INDEX "TechStackCategory_name_key";
-
--- AlterTable
-ALTER TABLE "TechStackCategory" ADD COLUMN     "voyageTeamId" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "TechStackCategory" ADD CONSTRAINT "TechStackCategory_voyageTeamId_fkey" FOREIGN KEY ("voyageTeamId") REFERENCES "VoyageTeam"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- prisma/migrations/20241020174120_category_new_teamid_column/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "TechStackCategory" ADD COLUMN     "voyageTeamIdNew" INTEGER;
-
--- AddForeignKey
-ALTER TABLE "TechStackCategory" ADD CONSTRAINT "TechStackCategory_voyageTeamIdNew_fkey" FOREIGN KEY ("voyageTeamIdNew") REFERENCES "VoyageTeam"("id") ON DELETE CASCADE ON UPDATE CASCADE;
