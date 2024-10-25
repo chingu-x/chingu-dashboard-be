031d9ca83a08325ce0ad7e9d1c0b056b8a636939
Arman Kumar Jena <arman.k.jena07@gmail.com>
2024-10-25 10:22:07 +0530

    added unit test for findFeatureCategories  service

--- src/features/features.service.spec.ts
+++ src/features/features.service.spec.ts
@@ -5,6 +5,7 @@ import { GlobalService } from "@/global/global.service";
 import { prismaMock } from "@/prisma/singleton";
 import { CustomRequest } from "@/global/types/CustomRequest";
 import { CreateFeatureDto } from "./dto/create-feature.dto";
+import { FeatureCategory, VoyageTeam } from "@prisma/client";
 /* import { UpdateFeatureDto } from "./dto/update-feature.dto";
 import { UpdateFeatureOrderAndCategoryDto } from "./dto/update-feature-order-and-category.dto"; */
 
@@ -44,6 +45,32 @@ const dtoCreateMock: CreateFeatureDto = {
     description: "Chingu is a global collaboration platform",
     featureCategoryId: 1,
 };
+
+const mockFeatureCategory = [
+    {
+        name: "must have",
+        id: 1,
+        description: "features that define your MVP",
+        createdAt: new Date(Date.now()),
+        updatedAt: new Date(Date.now()),
+    },
+    {
+        name: "should have",
+        id: 2,
+        description:
+            '"stretch goals" to be worked on when you’ve implemented all the "Must Haves"',
+        createdAt: new Date(Date.now()),
+        updatedAt: new Date(Date.now()),
+    },
+    {
+        name: "nice to have",
+        id: 3,
+        description:
+            '"stretch goals" to be worked on when you’ve implemented all the "Must Haves" and "Should Haves"',
+        createdAt: new Date(Date.now()),
+        updatedAt: new Date(Date.now()),
+    },
+];
 
 /* const dtoUpdateMock: UpdateFeatureDto = {
     teamMemberId: 1,
@@ -88,11 +115,11 @@ describe("FeaturesService", () => {
         it("should create a new feature", async () => {
             prismaMock.projectFeature.findFirst.mockResolvedValue(null);
             prismaMock.voyageTeam.findFirst.mockResolvedValue(
-                mockVoyageTeam as any,
+                mockVoyageTeam as VoyageTeam,
             );
             prismaMock.featureCategory.findFirst.mockResolvedValue({
                 id: 1,
-            } as any);
+            } as FeatureCategory);
             prismaMock.projectFeature.create.mockResolvedValue(mockFeature);
             mockGlobalService.getVoyageTeamMemberId.mockReturnValue(
                 mockTeamMemberId,
@@ -113,6 +140,30 @@ describe("FeaturesService", () => {
                     order: 1,
                 },
             });
+        });
+    });
+    describe("findFeatureCategories", () => {
+        it("findFeatureCategories service should be defined", async () => {
+            expect(service.findFeatureCategories).toBeDefined();
+        });
+
+        it("should return all feature categories", async () => {
+            prismaMock.featureCategory.findMany.mockResolvedValue(
+                mockFeatureCategory,
+            );
+
+            const result = await service.findFeatureCategories();
+
+            expect(result).toBeArray;
+            expect(result).toHaveLength(3);
+            expect(result[0]).toEqual({
+                name: expect.any(String),
+                id: expect.any(Number),
+                description: expect.any(String),
+                createdAt: expect.any(Date),
+                updatedAt: expect.any(Date),
+            });
+            expect(prismaMock.featureCategory.findMany).toHaveBeenCalled();
         });
     });
 });
