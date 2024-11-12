f033dac8c101dbdbacf89b2b7af8b15e1eeb58df
Curt <121463645+curtwl@users.noreply.github.com>
2024-11-11 23:36:44 -0600

    Add github-auth-service

---
+++ src/auth/github-auth-service.ts
@@ -0,0 +1,81 @@
+import {
+    Injectable,
+    Inject,
+    InternalServerErrorException,
+} from "@nestjs/common";
+import { IAuthProvider } from "../global/interfaces/oauth.interface";
+import { PrismaService } from "../prisma/prisma.service";
+import { GithubUser } from "../global/types/auth.types";
+import { generatePasswordHash } from "../global/auth/utils";
+import { OAuthConfig } from "@/config/Oauth/oauthConfig.interface";
+
+@Injectable()
+export class GithubAuthService implements IAuthProvider {
+    constructor(
+        private prisma: PrismaService,
+        @Inject("OAuth-Config") private oAuthConfig: OAuthConfig,
+    ) {}
+    async validateUser(user: GithubUser) {
+        const userInDb = await this.prisma.findUserByOAuthId(
+            "github",
+            user.githubId,
+        );
+
+        if (userInDb)
+            return {
+                id: userInDb.userId,
+                email: user.email,
+            };
+        return this.createUser(user);
+    }
+
+    async createUser(user: GithubUser) {
+        // generate a random password and not tell them so they can't login, but they will be able to reset password,
+        // and will be able to login with this in future,
+        // or maybe the app will prompt user the input the password, exact oauth flow is to be determined
+
+        // this should not happen when "user:email" is in the scope
+        if (!user.email)
+            throw new InternalServerErrorException(
+                "[github-auth.service]: Cannot get email from github to create a new Chingu account",
+            );
+
+        // check if email is in the database, add oauth profile to existing account, otherwise, create a new user account
+        return this.prisma.user.upsert({
+            where: {
+                email: user.email,
+            },
+            update: {
+                emailVerified: true,
+                oAuthProfiles: {
+                    create: {
+                        provider: {
+                            connect: {
+                                name: "github",
+                            },
+                        },
+                        providerUserId: user.githubId,
+                        providerUsername: user.username,
+                    },
+                },
+            },
+            create: {
+                email: user.email,
+                password: await generatePasswordHash(),
+                emailVerified: true,
+                avatar: user.avatar,
+                oAuthProfiles: {
+                    create: {
+                        provider: {
+                            connect: {
+                                name: "github",
+                            },
+                        },
+                        providerUserId: user.githubId,
+                        providerUsername: user.username,
+                    },
+                },
+            },
+        });
+    }
+}
