13dc3d3aaa8159f949f25abd678c6a5f60c88b69
Curt <121463645+curtwl@users.noreply.github.com>
2024-11-16 17:23:22 -0600

    Make error handling more robust

--- src/auth/github-auth.service.ts
+++ src/auth/github-auth.service.ts
@@ -2,8 +2,12 @@ import {
     Injectable,
     Inject,
     InternalServerErrorException,
+    NotFoundException,
 } from "@nestjs/common";
-import { IAuthProvider } from "../global/interfaces/oauth.interface";
+import {
+    AuthUserResult,
+    IAuthProvider,
+} from "../global/interfaces/oauth.interface";
 import { PrismaService } from "../prisma/prisma.service";
 import { GithubUser } from "../global/types/auth.types";
 import { generatePasswordHash } from "../global/auth/utils";
@@ -29,7 +33,7 @@ export class GithubAuthService implements IAuthProvider {
         return this.createUser(user);
     }
 
-    async createUser(user: GithubUser) {
+    async createUser(user: GithubUser): Promise<AuthUserResult> {
         // generate a random password and not tell them so they can't login, but they will be able to reset password,
         // and will be able to login with this in future,
         // or maybe the app will prompt user the input the password, exact oauth flow is to be determined
@@ -41,41 +45,57 @@ export class GithubAuthService implements IAuthProvider {
             );
 
         // check if email is in the database, add oauth profile to existing account, otherwise, create a new user account
-        return this.prisma.user.upsert({
-            where: {
-                email: user.email,
-            },
-            update: {
-                emailVerified: true,
-                oAuthProfiles: {
-                    create: {
-                        provider: {
-                            connect: {
-                                name: "github",
+        let upsertResult;
+        try {
+            upsertResult = await this.prisma.user.upsert({
+                where: {
+                    email: user.email,
+                },
+                update: {
+                    emailVerified: true,
+                    oAuthProfiles: {
+                        create: {
+                            provider: {
+                                connect: {
+                                    name: "github",
+                                },
                             },
+                            providerUserId: user.githubId,
+                            providerUsername: user.username,
                         },
-                        providerUserId: user.githubId,
-                        providerUsername: user.username,
                     },
                 },
-            },
-            create: {
-                email: user.email,
-                password: await generatePasswordHash(),
-                emailVerified: true,
-                avatar: user.avatar,
-                oAuthProfiles: {
-                    create: {
-                        provider: {
-                            connect: {
-                                name: "github",
+                create: {
+                    email: user.email,
+                    password: await generatePasswordHash(),
+                    emailVerified: true,
+                    avatar: user.avatar,
+                    oAuthProfiles: {
+                        create: {
+                            provider: {
+                                connect: {
+                                    name: "github",
+                                },
                             },
+                            providerUserId: user.githubId,
+                            providerUsername: user.username,
                         },
-                        providerUserId: user.githubId,
-                        providerUsername: user.username,
                     },
                 },
-            },
-        });
+            });
+        } catch (e) {
+            console.log("Error type:", e.constructor.name);
+            console.log("Error message:", e.message);
+            if (e.code === "P2025") {
+                if (e.message.includes("OAuthProvider")) {
+                    throw new NotFoundException(
+                        "OAuth provider not found in the database",
+                    );
+                }
+                console.error("Unexpected error during upsert:", e);
+                throw e;
+            }
+        }
+        return upsertResult;
     }
 }
