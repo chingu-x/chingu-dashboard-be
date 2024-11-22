27be692ac75efde3d3ce15fa52a2c0eb604533d8
Josh Hinman <joshuahinman@gmail.com>
2024-11-22 05:04:37 -0800

    updated controller spec

--- src/techs/techs.controller.spec.ts
+++ src/techs/techs.controller.spec.ts
@@ -1,25 +1,99 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { TechsController } from "./techs.controller";
 import { TechsService } from "./techs.service";
+//import { CustomRequest } from "@/global/types/CustomRequest";
 
 describe("TechsController", () => {
     let controller: TechsController;
 
+    //const requestMock = {} as unknown as CustomRequest;
+
+    const mockTechsService = {
+        getAllTechItemsByTeamId: jest.fn(() => {}),
+        addNewTeamTech: jest.fn(() => {}),
+        updateTeamTech: jest.fn(() => {}),
+        deleteTeamTech: jest.fn(() => {}),
+        addNewTechStackCategory: jest.fn(() => {}),
+        updateTechStackCategory: jest.fn(() => {}),
+        deleteTechStackCategory: jest.fn(() => {}),
+        addExistingTechVote: jest.fn(() => {}),
+        removeVote: jest.fn(() => {}),
+        updateTechStackSelections: jest.fn(() => {}),
+    };
+
     beforeEach(async () => {
         const module: TestingModule = await Test.createTestingModule({
             controllers: [TechsController],
-            providers: [
-                {
-                    provide: TechsService,
-                    useValue: {},
-                },
-            ],
-        }).compile();
+            providers: [TechsService],
+        })
+            .overrideProvider(TechsService)
+            .useValue(mockTechsService)
+            .compile();
 
         controller = module.get<TechsController>(TechsController);
     });
 
     it("should be defined", () => {
         expect(controller).toBeDefined();
+    });
+
+    describe("getAllTechItemsByTeamId", () => {
+        it("should be defined", () => {
+            expect(controller.getAllTechItemsByTeamId).toBeDefined();
+        });
+    });
+
+    describe("addNewTeamTech", () => {
+        it("should be defined", () => {
+            expect(controller.addNewTeamTech).toBeDefined();
+        });
+    });
+
+    describe("updateTeamTech", () => {
+        it("should be defined", () => {
+            expect(controller.updateTeamTech).toBeDefined();
+        });
+    });
+
+    describe("deleteTeamTech", () => {
+        it("should be defined", () => {
+            expect(controller.deleteTeamTech).toBeDefined();
+        });
+    });
+
+    describe("addNewTechStackCategory", () => {
+        it("should be defined", () => {
+            expect(controller.addNewTechStackCategory).toBeDefined();
+        });
+    });
+
+    describe("updateTechStackCategory", () => {
+        it("should be defined", () => {
+            expect(controller.updateTechStackCategory).toBeDefined();
+        });
+    });
+
+    describe("deleteTechStackCategory", () => {
+        it("should be defined", () => {
+            expect(controller.deleteTechStackCategory).toBeDefined();
+        });
+    });
+
+    describe("addExistingTechVote", () => {
+        it("should be defined", () => {
+            expect(controller.addExistingTechVote).toBeDefined();
+        });
+    });
+
+    describe("removeVote", () => {
+        it("should be defined", () => {
+            expect(controller.removeVote).toBeDefined();
+        });
+    });
+
+    describe("updateTechStackSelections", () => {
+        it("should be defined", () => {
+            expect(controller.updateTechStackSelections).toBeDefined();
+        });
     });
 });
