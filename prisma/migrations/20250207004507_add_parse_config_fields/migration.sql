0611def594c6fa00f7354cbc5ca4ab22cddbedab
timDeHof <ttdehof@gmail.com>
2025-02-07 11:29:40 -0500

    feat: Add parseConfig field to OptionChoice model

--- prisma/migrations/20250207004507_add_parse_config_fields/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "Form" ADD COLUMN     "parseConfig" JSONB;
-
--- AlterTable
-ALTER TABLE "OptionGroup" ADD COLUMN     "parseConfig" JSONB;
-
--- AlterTable
-ALTER TABLE "Question" ADD COLUMN     "parseConfig" JSONB;
