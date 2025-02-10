8767cf6e7eb31b26858b70de7b4c58309e0dc5a7
Cheryl M <webmaster@cherylli.com>
2025-02-10 22:06:31 +1100

    add swagger api summary

--- src/debug/debug.controller.ts
+++ src/debug/debug.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get, Param } from "@nestjs/common";
 import { DebugService } from "@/debug/debug.service";
+import { ApiOperation } from "@nestjs/swagger";
 
 @Controller("debug")
 export class DebugController {
     constructor(private readonly debugService: DebugService) {}
 
+    @ApiOperation({
+        summary: "Send onboardingEmail",
+    })
     @Get("test-onboarding/:email")
     sendOnboardingEmail(@Param("email") email: string) {
         return this.debugService.sendOnboardingEmail(email);
