1f8059b06b44a63ae197fb313ef32e9c19004210
Arman Kumar Jena <arman.k.jena07@gmail.com>
2025-03-13 23:30:29 +0530

    feat: Add new voyageProjectTeam role

--- prisma/seed/data/roles.ts
+++ prisma/seed/data/roles.ts
@@ -8,4 +8,8 @@ export default [
         description:
             "Advanced Projects - Apps having both Front-end and Back-end components (FULL STACK)",
     },
+    {
+        name: "voyageProjectTeam",
+        description: "Project belonging to a team in a voyage",
+    },
 ];
