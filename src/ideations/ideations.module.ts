3a625d53a98897307f5c39e373185063dfebdebf
timDeHof <ttdehof@gmail.com>
2025-03-24 20:23:47 -0400

    chore: removed unnecessary ideations module
    - Removed global validation pipe from ideations module to streamline provider configuration.

--- src/ideations/ideations.module.ts
+++ src/ideations/ideations.module.ts
@@ -2,15 +2,10 @@ import { Module } from "@nestjs/common";
 import { IdeationsService } from "./ideations.service";
 import { IdeationsController } from "./ideations.controller";
 import { PrismaModule } from "../prisma/prisma.module";
-import { APP_PIPE } from "@nestjs/core";
-import { ValidationPipe } from "@nestjs/common";
 
 @Module({
     controllers: [IdeationsController],
-    providers: [
-        IdeationsService,
-        { provide: APP_PIPE, useClass: ValidationPipe },
-    ],
+    providers: [IdeationsService],
     imports: [PrismaModule],
 })
 export class IdeationsModule {}
