61604d4c07d4c66761749e2da4de80120a68aeb2
Cheryl M <webmaster@cherylli.com>
2025-10-06 19:35:37 +1100

    chore: update script for prod seeding (#257)

--- prisma/production-seed/voyages.ts
+++ prisma/production-seed/voyages.ts
@@ -9,10 +9,10 @@ export const populateVoyagesProd = async () => {
                     name: "Active",
                 },
             },
-            startDate: new Date("2024-08-19T17:00:00.000Z"),
-            endDate: new Date("2024-09-29T04:59:59.000Z"),
-            soloProjectDeadline: new Date("2024-08-13T06:59:59.000Z"),
-            certificateIssueDate: new Date("2024-10-07T06:59:59.000Z"),
+            startDate: new Date("2025-09-29T17:00:00.000Z"),
+            endDate: new Date("2025-11-10T04:59:59.000Z"),
+            soloProjectDeadline: new Date("2025-09-23T06:59:59.000Z"),
+            certificateIssueDate: new Date("2025-11-18T06:59:59.000Z"),
         },
     });
     console.log("[Prod] voyages populated");
